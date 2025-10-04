3df6d15477025a4ba2b28a7e310052ae4287d424
Gerg-L <GregLeyda@proton.me>
2025-10-04 09:14:10 -0400

    feat: complete? lz.n api

--- framework.nix
+++ framework.nix
@@ -36,16 +36,45 @@ in
             lznOpts = lib.mkOption {
               type = lib.types.submodule {
                 options = {
-                  cmd = lib.mkOption {
+                  enabled = lib.mkOption {
                     type = lib.types.oneOf [
-                      (lib.types.listOf lib.types.str)
                       lib.types.str
+                      lib.types.bool
                     ];
+                    default = "";
+                  };
+                  beforeAll = lib.mkOption {
+                    type = lib.types.str;
+                    default = "";
+                  };
+                  before = lib.mkOption {
+                    type = lib.types.str;
                     default = "";
                   };
                   after = lib.mkOption {
                     type = lib.types.str;
                     default = "require('${config.name}').setup(${luaLib.toLuaObject config.setupOpts})";
+                  };
+                  event = lib.mkOption {
+                    type = lib.types.oneOf [
+                      (lib.types.listOf lib.types.str)
+                      lib.types.str
+                    ];
+                    default = [ ];
+                  };
+                  cmd = lib.mkOption {
+                    type = lib.types.oneOf [
+                      (lib.types.listOf lib.types.str)
+                      lib.types.str
+                    ];
+                    default = [ ];
+                  };
+                  ft = lib.mkOption {
+                    type = lib.types.oneOf [
+                      (lib.types.listOf lib.types.str)
+                      lib.types.str
+                    ];
+                    default = [ ];
                   };
                   keys = lib.mkOption {
                     type = lib.types.listOf (
@@ -55,24 +84,79 @@ in
                       ]
                     );
                   };
+                  colorscheme = lib.mkOption {
+                    type = lib.types.oneOf [
+                      (lib.types.listOf lib.types.str)
+                      lib.types.str
+                    ];
+                    default = [ ];
+                  };
+                  lazy = lib.mkOption {
+                    type = lib.types.bool;
+                    default = true;
+                  };
+                  priority = lib.mkOption {
+                    type = lib.types.int;
+                    default = 50;
+                  };
+                  load = lib.mkOption {
+                    type = lib.types.str;
+                    default = "";
+                  };
                 };
               };
             };
 
             textOutput = lib.mkOption {
               type = lib.types.lines;
-              default = ''
-                return {
-                  "${getName config.package}",
-                  cmd = ${lib.optionalString (config.lznOpts.cmd != "") "${luaLib.toLuaObject config.lznOpts.cmd}"},
-                  after = function()
-                    ${lib.optionalString (config.lznOpts.after != "")
-                      "${luaLib.toLuaObject (lib.mkLuaInline config.lznOpts.after)}"
-                    }
-                  end,
-                  keys = ${luaLib.toLuaObject config.lznOpts.keys},
-                }
-              '';
+              default =
+                let
+                  cfg = config.lznOpts;
+                in
+
+                ''
+                  return {
+                    "${getName config.package}",
+                    ${lib.optionalString (cfg.enabled != "") ''
+                      enabled = 
+                         ${
+                           if builtins.isBool cfg.enabled then
+                             luaLib.toLuaObject cfg.enabled
+                           else
+                             ''
+                               function()
+                               ${luaLib.toLuaObject (lib.mkLuaInline cfg.enabled)}
+                                  end
+                             ''
+                         },
+                    ''}
+                    ${lib.optionalString (cfg.beforeAll != "") ''
+                      beforeAll = function()
+                          ${luaLib.toLuaObject (lib.mkLuaInline cfg.beforeAll)}
+                      end,
+                    ''}
+                    ${lib.optionalString (cfg.before != "") ''
+                      before = function()
+                          ${luaLib.toLuaObject (lib.mkLuaInline cfg.before)}
+                      end,
+                    ''}
+                    ${lib.optionalString (cfg.after != "") ''
+                      after = function()
+                          ${luaLib.toLuaObject (lib.mkLuaInline cfg.after)}
+                      end,
+                    ''}
+                    ${lib.optionalString (cfg.event != [ ]) "event = ${luaLib.toLuaObject cfg.event},"}
+                    ${lib.optionalString (cfg.cmd != [ ]) "cmd = ${luaLib.toLuaObject cfg.cmd},"}
+                    ${lib.optionalString (cfg.ft != [ ]) "ft = ${luaLib.toLuaObject cfg.ft},"}
+                    ${lib.optionalString (cfg.keys != [ ]) "keys = ${luaLib.toLuaObject cfg.keys},"}
+                    ${lib.optionalString (
+                      cfg.colorscheme != [ ]
+                    ) "colorscheme = ${luaLib.toLuaObject cfg.colorscheme},"}
+                    lazy = ${luaLib.toLuaObject cfg.lazy},
+                    priority = ${luaLib.toLuaObject cfg.priority},
+                    ${lib.optionalString (cfg.load != "") "load = ${luaLib.toLuaObject (lib.mkLuaInline cfg.lazy)},"}
+                  }
+                '';
             };
           };
         }
